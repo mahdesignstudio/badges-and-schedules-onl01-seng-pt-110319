156251197718804632039bf946b4fc7012996020
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-11-10 03:32:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,7 @@ people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
+
+def batch_badge_creator(name)
+  return 
+end 
