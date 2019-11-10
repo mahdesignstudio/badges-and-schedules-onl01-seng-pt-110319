3b779174ffeb6f151c2302c3886bcde40a7dfc85
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-11-10 03:38:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator(attendees)
-  attendees = []
-  attendees.each do |name| 
-    attendees.push("Hello, my name is #{name}.")
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name| 
+    .push("Hello, my name is #{name}.")
   end 
   return attendees 
 end 
